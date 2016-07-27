0ef8f9d29f07452d5397454c41f5fac613828e89
heatherlauren <heatherlaurendavidson@gmail.com>
2016-07-27 12:03:22 +0100

    facebook login redirect works

--- src/require.js
+++ src/require.js
@@ -1,9 +1,10 @@
 const staticfile = require('./routes/staticfile.js')
 const homepage = require('./routes/homepage.js')
+const login = require('./routes/login.js')
 
 module.exports = {
   Hapi: require('hapi'),
   Handlebars: require('handlebars'),
   plugins: [require('inert'), require('vision')],
-  routes: [staticfile, homepage]
+  routes: [staticfile, homepage, login]
 }
