3e3222dd17c005fd841a8b7925ebbf876cba354c
heatherlauren <heatherlaurendavidson@gmail.com>
2016-07-27 17:35:03 +0100

    add session state and redirect to login
    CLOSES #1 CLOSES #9

--- src/routes/homepage.js
+++ src/routes/homepage.js
@@ -2,6 +2,9 @@ module.exports = {
   path: '/',
   method: 'GET',
   handler: (request, reply) => {
+    if (!request.state.token) {
+      return reply.redirect('login')
+    }
     reply.view('homepage')
   }
 }
