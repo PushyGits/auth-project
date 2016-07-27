847c7be0d722fbea6888764343ceb0a48564043e
heatherlauren <heatherlaurendavidson@gmail.com>
2016-07-27 11:04:05 +0100

    Set up server

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const req = require('./require.js')
+
+const server = new req.Hapi.Server()
+server.connection({ port: process.env.PORT || 3000 })
+
+server.start((err) => {
+  if (err) {
+    throw err
+  }
+  console.log('Server running at:', server.info.uri)
+})
+
+server.register(req.plugins, (err) => {
+  if (err) {
+    throw err
+  }
+
+  server.route(req.routes)
+
+  server.views({
+    engines: {html: req.Handlebars},
+    relativeTo: __dirname,
+    path: '../views/templates',
+    layout: 'default',
+    layoutPath: '../views/layout',
+    partialsPath: '../views/partials',
+    helpersPath: '../views/helpers'
+  })
+})
+
+module.exports = server
